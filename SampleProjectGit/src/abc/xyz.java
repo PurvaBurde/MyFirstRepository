854618c52b7d9a67f8e636a0622c430aa5286a56
Purva <Purva@192.168.1.192>
2022-03-18 18:47:03 +0530

    happy holi

--- SampleProjectGit/src/abc/xyz.java
+++ SampleProjectGit/src/abc/xyz.java
@@ -4,4 +4,9 @@ public class xyz {
 	public static void main(String[] args) {
 		System.out.println("This ts xyz class");
 	}
+	
+	public static void m1() {
+		System.out.println("git is mad");
+	}
 }
+
